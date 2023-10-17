f4c9bbb892f0f362175da55c6f6de3b124d5b689
camlink7 <camdenlink7@gmail.com>
2023-10-17 19:57:18 -0400

    added another dash to section

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>CS 230L</h1>
-      <h2>Section - 02</h2>
+      <h2>Section -- 02</h2>
       <p>WVU ID: 800381566</p>
       <p>Hi I am Camden</p>
     </div>
