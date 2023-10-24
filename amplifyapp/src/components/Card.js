01cefbe87a7e0b662963a24d3811cd609c693f91
camlink7 <camdenlink7@gmail.com>
2023-10-24 19:23:46 -0400

    made card headers h3 tags

--- amplifyapp/src/components/Card.js
+++ amplifyapp/src/components/Card.js
@@ -2,19 +2,19 @@ export default function Card(){
     return (
         <div className="d-flex container w-100 justify-content-center">
             <div class="card text-white bg-primary m-2" style={{maxWidth: "18rem"}}>
-                <div class="card-header">Card 01</div>
+                <h3 class="card-header">Card 01</h3>
                 <div class="card-body">
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
             <div class="card text-white bg-danger m-2" style={{maxWidth: "18rem"}}>
-                <div class="card-header">Card 02</div>
+                <h3 class="card-header">Card 02</h3>
                 <div class="card-body">
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
             <div class="card text-white bg-success m-2" style={{maxWidth: "18rem"}}>
-                <div class="card-header">Card 02</div>
+                <h3 class="card-header">Card 02</h3>
                 <div class="card-body">
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
